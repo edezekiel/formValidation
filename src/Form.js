ea8545afce2ba68735c36caceccc74a2077ba566
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-20 12:50:27 -0500

    input invalid styling only applies when element is in focus

--- src/Form.js
+++ src/Form.js
@@ -3,18 +3,26 @@ import React, { useState } from "react";
 function Form(props) {
   const [name, setName] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert(`Submitting Name ${name}`)
-  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (e.target.checkValidity()) {
+      alert(`Submitting Name ${name}`);
+    } else {
+      alert("invalid");
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} noValidate>
-      <label>
+      <label for="firstName">
         First Name:
         <input
           type="text"
+          id="firstName"
+          name="txtFName"
           value={name}
           onChange={e => setName(e.target.value)}
+          required
         />
       </label>
       <input type="submit" value="Submit" />
